fa7b05fcb3b49dad92cb3f1f7c435e9085b387f3
Junha0Zhang <100944334+Junha0Zhang@users.noreply.github.com>
2022-06-13 16:16:12 +0200

    Add files via upload

---
+++ Python/global_registration.py
@@ -0,0 +1,91 @@
+import os
+import numpy as np
+import open3d as o3d
+import copy
+
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp])
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+def prepare_dataset(voxel_size):
+    print(":: Load two point clouds and disturb initial pose.")
+
+    source = o3d.io.read_point_cloud("multiway_registered_1st.ply")
+    target = o3d.io.read_point_cloud("mri_regi_transformed.ply")
+    trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
+                             [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+    source.transform(trans_init)
+    draw_registration_result(source, target, np.identity(4))
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+
+voxel_size = 0.005  # means 5cm for this dataset
+source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(
+    voxel_size)
+
+def execute_fast_global_registration(source_down, target_down, source_fpfh,
+                                     target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.5
+    print(":: Apply fast global registration with distance threshold %.3f" \
+            % distance_threshold)
+    result = o3d.pipelines.registration.registration_fast_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh,
+        o3d.pipelines.registration.FastGlobalRegistrationOption(
+            maximum_correspondence_distance=distance_threshold))
+    return result
+
+result_fast = execute_fast_global_registration(source_down, target_down,
+                                               source_fpfh, target_fpfh,
+                                               voxel_size)
+print(result_fast)
+draw_registration_result(source_down, target_down, result_fast.transformation)
+
+def refine_registration(source, target, source_fpfh, target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.4
+    print(":: Point-to-plane ICP registration used")
+
+    print("   distance threshold %.3f." % distance_threshold)
+    result = o3d.pipelines.registration.registration_icp(
+        source, target, distance_threshold, result_fast.transformation,
+        o3d.pipelines.registration.TransformationEstimationPointToPlane())
+    return result
+
+result_icp = refine_registration(source, target, source_fpfh, target_fpfh,
+                                 voxel_size)
+print(result_icp)
+draw_registration_result(source, target, result_icp.transformation)
+
+# refine registration
+source_transformed = source.transform(result_icp.transformation)
+dists = source_transformed.compute_point_cloud_distance(target)
+dists = np.asarray(dists)
+ind = np.where(dists < 0.015)[0]
+scanned_only_mri = source.select_by_index(ind)
+print('final result')
+o3d.visualization.draw_geometries([scanned_only_mri, target])
+
+o3d.io.write_point_cloud("only_mri_multiway_registration.ply" , scanned_only_mri)
