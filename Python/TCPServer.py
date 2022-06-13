7813490cef1d626b731a633d85592fc720bd2664
Juyi Mejiro Zhang <100589897+JuyiZhang@users.noreply.github.com>
2022-06-13 19:09:33 +0200

    Add files via upload

--- Python/TCPServer.py
+++ Python/TCPServer.py
@@ -52,30 +52,8 @@ def tcp_server():
             print('--------------------------\nHeader: ' + header)
             print(len(data))
 
-            if header == 's':
-                # save depth sensor images
-                data_length = struct.unpack(">i", data[1:5])[0]
-                N = data_length
-                depth_img_np = np.frombuffer(data[5:5+N], np.uint16).reshape((512,512))
-                ab_img_np = np.frombuffer(data[5+N:5+2*N], np.uint16).reshape((512,512))
-                timestamp = str(int(time.time()))
-                cv2.imwrite(save_folder + timestamp+'_depth.tiff', depth_img_np)
-                cv2.imwrite(save_folder + timestamp+'_abImage.tiff', ab_img_np)
-                print('Image with ts ' + timestamp + ' is saved')
-            if header == 'f':
-                # save spatial camera images
-                data_length = struct.unpack(">i", data[1:5])[0]
-                ts_left, ts_right = struct.unpack(">qq", data[5:21])
-
-                N = int(data_length/2)
-                LF_img_np = np.frombuffer(data[21:21+N], np.uint8).reshape((480,640))
-                RF_img_np = np.frombuffer(data[21+N:21+2*N], np.uint8).reshape((480,640))
-                cv2.imwrite(save_folder + str(ts_left)+'_LF.tiff', LF_img_np)
-                cv2.imwrite(save_folder + str(ts_right)+'_RF.tiff', RF_img_np)
-                print('Image with ts %d and %d is saved' % (ts_left, ts_right))
             if header == 'p':
                 # save point cloud
-                # print(data)
                 timestamp = struct.unpack(">q", data[1:9])[0]
                 N_pointcloud = struct.unpack(">q", data[9:17])[0]
                 print("Length of point cloud:" + str(N_pointcloud))
@@ -84,21 +62,17 @@ def tcp_server():
                 filename = "PointCloudCapture/" + timestamp + "_pc.ndarray"
                 print(filename)
                 np.save(filename, pointcloud_np, allow_pickle=True, fix_imports=True)
+
+                # Uncomment this line and obtain transformation matrix from other codes
                 #transformationMatrix = np.float32([2.5,1.5,1.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0])
                 #conn.send(transformationMatrix.tobytes())
-                #Reconstruction data, remove comment in the future
-                """temp_filename_pc = timestamp + '_pc.ply'
-                #print(pointcloud_np.shape)
-                #o3d_pc = o3d.geometry.PointCloud()
-                #o3d_pc.points = o3d.utility.Vector3dVector(pointcloud_np.astype(np.float64))
-                #o3d.io.write_point_cloud(save_folder + temp_filename_pc, o3d_pc, write_ascii=True)
-                #print('Saved  image to ' + temp_filename_pc)"""
+                
             if header == "i":
                 # save RGB Image
                 print("Saving RGB Image")
                 timestamp = struct.unpack(">q", data[1:9])[0]
                 N_image = struct.unpack(">q", data[9:17])[0]
-                print("Length of image:" + str(N_image))
+                print("Length of point cloud:" + str(N_image))
                 #image_np = np.frombuffer(data[17:17+N_image*4], np.uint8).reshape((-1,3))
                 timestamp = str(int(time.time()*1000))
                 filename = "ImageCapture/" + timestamp + "_pc.imgdata"
